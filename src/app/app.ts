efa83c201054933a96d4ce6fe4f687e35468278c
FilipaBarreto <replayandreplay@gmail.com>
2025-11-14 12:06:18 +0000

    inicio do projecto no exemplo1- praticando

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('filipa');
+  protected readonly title = signal('exemplo1');
 }
